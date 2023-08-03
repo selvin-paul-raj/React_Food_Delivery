4495174454b388a7692a32ad2a530888f7706440
SPRHackz <sprhackz003@gmail.com>
2023-08-03 20:40:32 +0530

    First Commit Of The React-Food-Delivery-App

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "remixicon/fonts/remixicon.css";
+import "./index.css";
+
+import store from "./store/store";
+import { Provider } from "react-redux";
+
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
